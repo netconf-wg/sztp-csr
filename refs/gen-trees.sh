0fb0b31da6bddbc31620e0291f8134687abec072
kwatsen <kent@watsen.net>
2021-12-13 14:31:27 -0500

    updates per Éric Vyncke's IESG ballot

--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -1,18 +1,8 @@
 echo "Generating tree diagrams..."
 
 # print just the augment tree (yanglint's output is less cluttered)
-pyang -f tree --tree-line-length 69 -p ../ ../ietf-sztp-csr\@*.yang > ietf-sztp-csr-tree.txt 
+pyang -f tree --tree-line-length 69 --tree-print-structures -p ../ ../ietf-sztp-csr\@*.yang > ietf-sztp-csr-tree.txt
 pyang -f tree --tree-line-length 69 --tree-print-groupings ../ietf-ztp-types\@*.yang > ietf-ztp-types-tree.txt
-
-
-# append the sx:structure to the module tree (requires script-fu!)
-name=`ls -1 ../ietf-sztp-csr\@*.yang`
-mv $name $name.sav
-sed -e 's/sx:structure/container/' -e 's/csr-request {/csr-request { config false;/' $name.sav > $name
-#echo "" > ietf-sztp-csr-tree.txt
-pyang -W none -p ../ -f tree $name | sed -e '/module/G' -e 's/+--ro csr-request/structure: csr-request/' >> ietf-sztp-csr-tree.txt
-#yanglint -p ../ -f tree --tree-path /csr-request $name | sed -e '/module/d' -e 's/+--ro csr-request/structure: csr-request/' >> ietf-sztp-csr-tree.txt
-mv $name.sav $name
 
 # print the get-bootstrapping-data rpc + augments
 pyang -p ../ -f tree --tree-path /get-bootstrapping-data ietf-sztp-bootstrap-server\@*.yang ../ietf-sztp-csr\@*.yang > ietf-sztp-api-n-csr-tree.txt 
