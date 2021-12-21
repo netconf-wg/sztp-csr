ee5b28d35ec26c895ce4691f85affd1487d51f3e
kwatsen <kent@watsen.net>
2021-12-21 12:43:48 -0500

    address Ben Kaduk's discuss

--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -9,13 +9,14 @@ pyang -p ../ -f tree --tree-path /get-bootstrapping-data ietf-sztp-bootstrap-ser
 sed 's/reporting-level?        /reporting-level?    /' ietf-sztp-api-n-csr-tree.txt > tmp;
 mv tmp ietf-sztp-api-n-csr-tree.txt
 
-# print rpc-errors + structure
-sed -e 's/sx:structure/grouping g { container/' -e 's/^}/}}/' ../ietf-sztp-csr\@*.yang > ietf-sztp-csr.yang
-sed -e 's/prefix "rc";/prefix "rc"; import ietf-sztp-csr { prefix sztp-csr;}/' -e "s/2017-01-26/`date +%Y-%m-%d`/" -e 's/anydata error-info {/container error-info { uses sztp-csr:g;/' -e 's/^}/uses errors { refine errors { config false; }}}/' ietf-restconf\@2017-01-26.yang > ietf-restconf\@`date +%Y-%m-%d`.yang
+# print rpc-errors + structure (ensure "sztp-csr:" prefix appears)
+sed -e 's/prefix sztp-csr;/prefix sztp-csr; import ietf-restconf { prefix rc; }/' -e 's#sx:structure csr-request {#augment "/rc:errors/rc:error/rc:error-info" { container csr-request {#' -e 's/uses zt:csr-request-grouping;/uses zt:csr-request-grouping; }/' ../ietf-sztp-csr\@*.yang > ietf-sztp-csr.yang
+sed -e "s/2017-01-26/`date +%Y-%m-%d`/" -e 's/anydata error-info {/container error-info {/' -e 's/^}/uses errors { refine errors { config false; }}}/' ietf-restconf\@2017-01-26.yang > ietf-restconf\@`date +%Y-%m-%d`.yang
 cp ../ietf-ztp-types\@*.yang .
-pyang -f tree --tree-path /errors ietf-restconf\@`date +%Y-%m-%d`.yang > ietf-sztp-csr-errors-n-struct-tree.txt
+pyang -f tree --tree-path /errors ietf-restconf\@`date +%Y-%m-%d`.yang ietf-sztp-csr.yang | ghead -n -5  > ietf-sztp-csr-errors-n-struct-tree.txt
 #rm ietf-sztp-csr.yang
 #rm ietf-ztp-types\@*.yang
+
 
 
 
